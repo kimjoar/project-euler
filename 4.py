cacef57c3885ef8a8c2992fdc7cd518a67a04a4a
Kim Joar Bekkelund <mail@kimjoar.net>
2009-04-04 16:11:42 +0200

    Added new solution to 4, but it was actually slower than the initial solution.

--- 4.py
+++ 4.py
@@ -1,7 +1,21 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-def isPalindrome(int):
-  return (str(int) == str(int)[::-1])
+# Solution 1
+def isPalindrome(number):
+  return (str(number) == str(number)[::-1])
+
+# Solution 2
+def isPalindrome2(number):
+  number = str(number)
+  length = len(number)
+  
+  for i in range(length / 2):
+    if number[i] != number[length - (i + 1)]:
+      return False
+
+  return True
+
+# Performs computation
 
 largest = 0
 for i in xrange(100, 1000):
@@ -10,3 +24,8 @@ for i in xrange(100, 1000):
       largest = i * j
 
 print largest
+
+# I initially thought that isPalindrome2 would be faster than isPalindrome, but
+# to my suprise isPalindrome is faster (atleast on the machine I tested it on).
+# On the other hand it performs slower than isPalindrome2 if I do:
+#   return (number == int(str(number)[::-1]))
