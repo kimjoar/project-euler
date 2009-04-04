ec5445071fdae476b90ac73f8251bb990e7f2a2b
Kim Joar Bekkelund <mail@kimjoar.net>
2009-04-04 16:45:16 +0200

    Cleaning up

--- 7.py
+++ 7.py
@@ -1,12 +1,7 @@
 # Find the 10001st prime.
 
 def isprime(n):
-  # Not fully correct isprime implementation because it doesn't care about 
-  # numbers smaller than 13, and only receives odd numbers
-
-  # Range starts with 3 and only needs to go up the squareroot of n
-  # for all odd numbers
-
+  # Not fully correct isprime implementation, e.g. always receives odd numbers.
   # It might be better to implement the sieve of Eratosthenes to solve this 
   # problem more effectively.
   for x in range(3, int(n ** 0.5) + 1, 2):
@@ -14,14 +9,12 @@ def isprime(n):
       return False
   return True
 
-prime, n, value = 13, 6, 15
-n = 6
+prime, n, value = 3, 2, 3
 
-while n < 10001:
+while(n < 10001):
+  value += 2
   if isprime(value):
     prime = value
     n += 1
-  value += 2
-
+    
 print prime
-
