caafc936a1ad238ea163f50a3207975815f5b25e
Kim Joar Bekkelund <mail@kimjoar.net>
2009-04-04 16:33:20 +0200

    Added a must faster algorithm for 5.

--- 5.py
+++ 5.py
@@ -1,10 +1,25 @@
 # What is the smallest number that is evenly divisible by all the numbers from 1 to 20?
 
 def divisable(n):
-  return bool(sum([n % i for i in range(1,21)]) == 0)
+  return sum([n % i for i in range(1,21)]) == 0
 
-n = 2520
-while not divisable(n):
+def divisable2(n):
+  for i in range(1,21):
+    if n % i != 0:
+      return False
+  return True
+
+# We don't need to check the numbers that divide a number already checked.
+# Therefore we don't need to check 5 and 10 since we already must check 20, and
+# so on for the other numbers. We therefore only need to check 11 through 20.
+def divisable3(n):
+  for i in range(11,21):
+    if n % i != 0:
+      return False
+  return True
+
+n = 20
+while not divisable3(n):
   n += 20
 
 print n
